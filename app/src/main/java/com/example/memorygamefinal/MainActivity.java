ee53b16df7e35e8ca09952f723f777b79e48f917
himansig7 <himansig7@gmail.com>
2023-05-22 14:22:32 -0700

    x

--- app/src/main/java/com/example/memorygamefinal/MainActivity.java
+++ app/src/main/java/com/example/memorygamefinal/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.memorygamefinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button startButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               openGameScreen();
+            }
+        });
+
+
+    }
+    public void openGameScreen(){
+        Intent intent = new Intent(this, gameScreen.class);
+        startActivity(intent);
     }
 }
